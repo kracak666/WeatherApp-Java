d11999fd554fa7bfd997703cbd5a908a722d013e
kracak <scislokuba@gmail.com>
2024-01-09 15:16:16 +0100

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.weatherapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.weatherapp to javafx.fxml;
+    exports com.example.weatherapp;
+}
